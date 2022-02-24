ba231b8971563dc0b960c90669fe90e7399d84a8
Fabiano Vilela <fabiano.vilela@gmail.com>
2022-02-23 22:37:13 -0300

    Refactor. Adding test for Clear list

--- src/test/List.spec.tsx
+++ src/test/List.spec.tsx
@@ -1,44 +1,57 @@
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen,  } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import List from '../components/List'
 
 describe('List component', () => {
   it('should render list items', async () => {
-    const { getByText, queryByText, rerender, unmount } = render(<List initialItems={['Fabiano', 'Vilela']} />)
+    const { rerender, unmount } = render(<List initialItems={['Fabiano', 'Vilela']} />)
 
-    expect(getByText('Fabiano')).toBeInTheDocument();
+    expect(screen.getByText('Fabiano')).toBeInTheDocument();
 
     unmount();
     rerender(<List initialItems={['Fabiano', 'Silva']}/>);
 
-    expect(queryByText('Vilela')).not.toBeInTheDocument();
-    expect(queryByText('Silva')).toBeInTheDocument();
+    expect(screen.queryByText('Vilela')).not.toBeInTheDocument();
+    expect(screen.queryByText('Silva')).toBeInTheDocument();
   })
 
   it('should be able to add new item to the list', async () => {
-    const { getByText, getByTestId } = render(<List initialItems={[]}/>);
-    const addButton = getByText('Add');
+    render(<List initialItems={[]} />);
+    const addButton = screen.getByText('Add');
 
-    const input = getByTestId('item-id');
+    const input = screen.getByTestId('item-id');
     userEvent.type(input, 'Silva');
 
     fireEvent.click(addButton);
 
     await waitFor(() => {
-      expect(getByText('Silva')).toBeInTheDocument();
+      expect(screen.getByText('Silva')).toBeInTheDocument();
     })
   })
 
   it('should be able to remove item from the list', async () => {
-    const { getAllByText, queryByText } = render(<List initialItems={['Fabiano', 'Vilela']}/>);
+    render(<List initialItems={['Fabiano', 'Vilela']} />);
 
-    const removeButtons = getAllByText('Remove');
+    const removeButtons = screen.getAllByText('Remove');
 
     fireEvent.click(removeButtons[0]);
 
     await waitFor(() => {
-      expect(queryByText('Fabiano')).not.toBeInTheDocument();
+      expect(screen.queryByText('Fabiano')).not.toBeInTheDocument();
+    })
+  })
+
+  it('should be able to clear the list', async () => {
+    render(<List initialItems={['Fabiano', 'Vilela']} />);
+
+    const clearButton = screen.getByText('Clear');
+
+    fireEvent.click(clearButton);
+
+    await waitFor(() => {
+      expect(screen.queryByText('Fabiano')).not.toBeInTheDocument();
+      expect(screen.queryByText('Vilela')).not.toBeInTheDocument();
     })
   })
 })
